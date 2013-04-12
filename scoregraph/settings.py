94cbc4962f15dcea4d2535df1c4d5c44b244a03f
Jonathan Goldsmith <jhghank@gmail.com>
2013-04-11 19:10:38 -0700

    changed formatting

--- scoregraph/settings.py
+++ scoregraph/settings.py
@@ -60,6 +60,9 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
+
+#/Users/jonathangoldsmith/projects/scoregraph/scoregraph/ for local
+#static for heroku
 STATIC_ROOT = 'static'
 
 # URL prefix for static files.
